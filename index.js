9e610a6e25714f57b9ddd120950c913799bc9b27
rithiv7 <rithivaithya07@gmail.com>
2018-07-11 15:41:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // Write your code in this file!
+var currentUser ="rithi"
 
+var welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
+
+var excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, RITHI!"
+var shortGreeting = "Welcome, r!"
