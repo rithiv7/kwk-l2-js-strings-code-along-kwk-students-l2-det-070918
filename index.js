a99ca4d516f8f99bfaff70254e860aa96d27c5a3
rithiv7 <rithivaithya07@gmail.com>
2018-07-11 15:37:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 var currentUser ="rithi"
 
-var welcomeMessage = "Welcome to Klossybook, " + currentUser + 
+var welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
 
